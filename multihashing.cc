6b5e1bdbe61a2863f6360844a9f276340c838e3b
zzzkky <philip.20050132@163.com>
2020-05-17 19:56:16 +0800

    add safex

--- multihashing.cc
+++ multihashing.cc
@@ -103,6 +103,9 @@ void init_rx(const uint8_t* seed_hash_data, xmrig::Algorithm::Id algo) {
             case 19:
                 randomx_apply_config(RandomX_KevaConfig);
                 break;
+            case 20:
+                randomx_apply_config(RandomX_SafexConfig);
+                break;
             default:
                 throw std::domain_error("Unknown RandomX algo");
         }
